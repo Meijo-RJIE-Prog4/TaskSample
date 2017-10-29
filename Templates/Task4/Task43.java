762c9dfb3bad04ca6991019480cda3c2c95d1ded
Hidekazu Suzuki <hsuzuki@ucl.meijo-u.ac.jp>
2017-10-29 21:13:26 +0900

    第4回課題の雛型を追加

---
+++ Templates/Task4/Task43.java
@@ -0,0 +1,36 @@
+// rpgパッケージに所属させる（消さないこと！）
+package rpg;
+
+// 必要なクラスをインポート
+
+
+/**
+ * 基本課題4.3（雛形）
+ */
+public class Task43 {
+
+	public static void main(String[] args) {
+		// 発生させるモンスターの数を2〜5の範囲で生成する
+		
+		// 生成するモンスターを格納する配列を作成
+		
+		// モンスターをランダムに生成
+
+		// 生成したモンスターを表示
+		showMonsters(XXX);
+		
+		// モンスターがランダムに行動
+
+		// 全てのモンスターが逃げ出したら表示
+		System.out.println("\nモンスターは全て逃げ出した！");
+	}
+
+	/**
+	 * モンスターの一覧を表示するクラスメソッド
+	 * @param XXX	モンスターを管理している配列（型と引き数名を設定すること）
+	 */
+	private static void showMonsters(XXX) {
+
+		System.out.println("が現れた！\n");
+	}
+}
