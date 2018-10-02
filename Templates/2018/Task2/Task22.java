9d09684f0ab4cf29ca1bb010fd6c4d3cbc264f76
Hidekazu Suzuki <hsuzuki@ucl.meijo-u.ac.jp>
2018-10-02 22:00:01 +0900

    2018年度第2回課題の雛型を作成

---
+++ Templates/2018/Task2/Task22.java
@@ -0,0 +1,28 @@
+/** 基本課題2.2　動作確認クラス雛型 */
+public class Task22 {
+
+	/**
+	 * メインメソッド
+	 * @param args	X座標とY座標
+	 */
+	public static void main(String[] args) {
+		// 点P1を作成してコマンドライン引数で指定した座標を設定
+		
+
+		// 情報の表示
+		System.out.println("点P1(X, Y)を生成しました．");
+		System.out.println("X座標=X，Y座標=Y");
+		// 原点とP1間の距離を表示
+		
+
+		// 点P2を作成してキーボードで指定した座標を設定
+		
+		System.out.print("P2の座標を入力してください> ");
+		
+
+
+		// P1-P2間の距離を表示
+		System.out.println("P1-P2間の距離=D");
+	}
+
+}
