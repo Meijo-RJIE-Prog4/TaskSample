67ce9dd24ab0f20bc8270a20975da78bffafce56
Hidekazu Suzuki <hsuzuki@ucl.meijo-u.ac.jp>
2019-09-25 19:43:05 +0900

    2019年度版の基本課題2.3雛型を作成

---
+++ Task2/Task23.java
@@ -0,0 +1,26 @@
+/** 
+ * 基本課題2.3　動作確認クラス
+ * @author 180441xxx　情報太郎
+ */
+
+public class Task23 {
+	public static void main(String[] args) {
+		/* 1. キーボードで入力した氏名と電話番号を用いてスマートカードを作成 */
+		System.out.println("スマートカードを作成します．");
+				
+
+		System.out.println("スマートカードを作成しました．\n");
+
+		/* 2. カード残高を表示する */
+		System.out.println("スマートカードの情報を表示します．");
+		
+		/* 3. キーボードで入力した金額を支払おうとするが残高不足のため，チャージして支払う */
+		System.out.print("支払金額を入力してください> ");
+
+		
+		/* 4. カード残高を表示する */
+		System.out.println("スマートカードの情報を表示します．");
+		
+	}
+
+}
